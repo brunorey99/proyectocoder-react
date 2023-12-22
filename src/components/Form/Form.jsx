4716531ce1c865bead22ea696bbef6f49d210140
Bruno Reynoso <brunoreynoso1999@gmail.com>
2023-12-21 23:56:38 -0300

    form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,27 +1,32 @@
+import { Box, Button, Stack, TextField } from "@mui/material";
 import { useContext, useState } from "react";
 import { FirebaseContext } from "../../context/FirebaseContext";
 import { CartContext } from "../../context/CartContext";
 
 export const Form = () => {
     const { addOrderDB } = useContext(FirebaseContext);
-    const {cartItems, totalCartItems } = useContext(CartContext);
+    const { cartItems, totalCartItems } = useContext(CartContext);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
-  
+
     const handleForm = (e) => {
-      e.preventDefault();
-  
-      addOrderDB(cartItems, {name, email}, totalCartItems);
-      
-      setName("");
-      setEmail("");
+        e.preventDefault();
+
+        addOrderDB(cartItems, { name, email }, totalCartItems);
+
+        setName("");
+        setEmail("");
     };
-  
+
     return (
-        <div className="d-flex flex-column w-25" onSubmit={handleForm}>
-            <input type="text" value={name} placeholder="Nombre" onChange={(e) => setName(e.target.value)} />
-            <input type="email" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-            <button type="submit" className="btn btn-success">Comprar</button>
-        </div>
+        <Box display={"flex"} flexDirection={"column"} width={400} component="form" onSubmit={handleForm}>
+            <Stack spacing={3}>
+                <TextField label={"Nombre"} value={name} onChange={(e) => setName(e.target.value)} />
+                <TextField label={"Email"} value={email} onChange={(e) => setEmail(e.target.value)} />
+                <Button type="submit" variant="contained" color="success">
+                    Comprar
+                </Button>
+            </Stack>
+        </Box>
     );
-  };
+};
